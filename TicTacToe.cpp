0006321e5eaf29a222a9cac263c075bc574a7856
Themiya Ehalawela <themiyaehalawela2005@gmail.com>
2021-03-07 21:13:18 +0530

    Basic Layout

---
+++ TicTacToe.cpp
@@ -0,0 +1,226 @@
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+#define CLS() (system("cls"))
+#define PAUSE() (system("pause"))
+#define SINGLE_PLAYER_MODE 1
+#define TWO_PLAYER_MODE 2
+#define OPPONENT 1
+#define PLAYER 2
+
+class TicTacToe : public olc::PixelGameEngine
+{
+public:
+    TicTacToe()
+    {
+        sAppName = "Tic Tac Toe";
+    }
+
+    bool OnUserCreate() override
+    {
+        return true;
+    }
+
+    bool OnUserUpdate(float fElapsedTime) override
+    {
+        Clear(olc::BLACK);
+        DrawLine(10, 10, 590, 10);
+        return true;
+    }
+
+private:
+    short board[3][3] = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0},
+    };
+    short PLAYER_ONE = 1;
+    short PLAYER_TWO = 2;
+    short *CURRENT_PLAYER = &PLAYER_ONE;
+
+    // TODO: implement Dispaly Board
+    // TODO: implement getInput function
+
+    short isFree(short x, short y)
+    {
+        /* Check if a certain position on the game board is free for a certain player
+	if the position is free, it will return 1, if not will return 0 */
+        return (!board[x][y]) ? 1 : 0;
+    }
+
+    short movePlayer(short x, short y)
+    {
+        /* Move the current player to the specified opsition on the board
+	returns -1 if the position is already taken by another player */
+        if (isFree(x, y) && x < 3 && y < 3 && x >= 0 && y >= 0)
+        {
+            board[x][y] = *CURRENT_PLAYER;
+            return 0;
+        }
+        return -1;
+    }
+
+    void switchPlayer()
+    {
+        /* Switch the current player */
+        if (CURRENT_PLAYER == &PLAYER_ONE)
+        {
+            CURRENT_PLAYER = &PLAYER_TWO;
+        }
+        else if (CURRENT_PLAYER == &PLAYER_TWO)
+        {
+            CURRENT_PLAYER = &PLAYER_ONE;
+        }
+    }
+
+    short isBoardFilled()
+    {
+        /* Check if a tie situation has come to `play` */
+        for (short row = 0; row < 3; row++)
+        {
+            for (short i = 0; i < 3; i++)
+            {
+                if (!board[row][i])
+                {
+                    return 0;
+                }
+            }
+        }
+        return 1;
+    }
+
+    short hasWon()
+    {
+        /* Check the game for any winners */
+        // Go through 1 and 2, the players
+        for (short player = 1; player < 3; player++)
+        {
+            // Search the rows for winners
+            for (short row = 0; row < 3; row++)
+            {
+                if (board[row][0] == player && board[row][1] == player && board[row][2] == player)
+                {
+                    return player;
+                }
+            }
+            // search each column
+            for (short col = 0; col < 4; col++)
+            {
+                if (board[0][col] == player && board[1][col] == player && board[2][col] == player)
+                {
+                    return player;
+                }
+            }
+            // Search the cross sections
+            if (board[1][1] == player)
+            {
+                if (board[0][0] == player && board[2][2] == player)
+                {
+                    return player;
+                }
+                if (board[0][2] == player && board[2][0] == player)
+                {
+                    return player;
+                }
+            }
+        }
+        return 0;
+    }
+
+    int evaluateBoard()
+    {
+        /* Evaluate the board for the usage of the minmax function */
+        switch (hasWon())
+        {
+        case OPPONENT:
+            return -10;
+        case PLAYER:
+            return 10;
+        case 0:
+            return 0;
+        default:
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    int minMax(int depth)
+    {
+        /* considers all the possible ways the game can go and return the value of the board */
+        int score = evaluateBoard();
+        if (score == 10 && score == -10)
+            return score;
+        if (isBoardFilled())
+            return 0;
+
+        if (CURRENT_PLAYER == &PLAYER_TWO)
+        {
+            int best = -1000;
+            for (short row = 0; row < 3; row++)
+            {
+                for (short col = 0; col < 3; col++)
+                {
+                    if (isFree(row, col))
+                    {
+                        movePlayer(row, col);
+                        best = std::max(best, minMax(depth + 1));
+                        board[row][col] = 0;
+                    }
+                }
+            }
+            return best;
+        }
+
+        else
+        {
+            int best = 1000;
+            for (short row = 0; row < 3; row++)
+            {
+                for (short col = 0; col < 3; col++)
+                {
+                    if (isFree(row, col))
+                    {
+                        movePlayer(row, col);
+                        best = std::min(best, minMax(depth + 1));
+                    }
+                }
+            }
+            return best;
+        }
+    }
+
+    void findBestMove(short *x, short *y)
+    {
+        /* Find the optimal move for the computer player, using the min_max algorithm and recursion */
+        int bestVal = -1000;
+        *x = -1;
+        *y = 1;
+        // move through every position of the board and try the spots
+        for (int row = 0; row < 3; row++)
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                if (isFree(row, col))
+                {
+                    movePlayer(row, col);
+                    int moveVal = minMax(0);
+                    board[row][col] = 0;
+
+                    if (moveVal > bestVal)
+                    {
+                        *x = row;
+                        *y = col;
+                        bestVal = moveVal;
+                    }
+                }
+            }
+        }
+    }
+};
+
+int main(int argc, char** argv)
+{
+    TicTacToe tic;
+    if (tic.Construct(600, 600, 1, 1))
+        tic.Start();
+    return 0;
+}
